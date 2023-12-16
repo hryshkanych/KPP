7cd0cfc6245c4b1745b3ccf805fe9159d7ce2df9
hryshkanych <118569670+hryshkanych@users.noreply.github.com>
2023-12-16 15:11:09 +0200

    add lab7

---
+++ Lab7_Bank/Lab7_Bank/KPP.Lab7/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.lab7 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    //requires eu.hansolo.tilesfx;
+
+    opens com.example.lab7 to javafx.fxml;
+    exports com.example.lab7;
+}
